a3fd2d6038c48a32d41f941660e394bdcf043f15
Mikhail Markov <mikhaiil@mmarkovv.ru>
2024-02-15 21:35:06 +0300

    Saving msges to db

--- messanger/consumers.py
+++ messanger/consumers.py
@@ -3,6 +3,8 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from messanger import models
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
@@ -22,19 +24,28 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        sender = text_data_json['sender']
-        receiver = text_data_json['receiver']
+        sender = text_data_json['sender_id']
+        receiver = text_data_json['receiver_id']
+        msg = models.Message(
+            sender_id=models.MyUser.objects.get(id=sender),
+            receiver_id=models.MyUser.objects.get(id=receiver),
+            title=message[:30],
+            content=message,
+            status='sent',
+        )
+        msg.save()
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat_message",
                                    "message": message,
-                                   'sender': sender,
-                                   'receiver': receiver}
+                                   'sender_id': sender,
+                                   'receiver_id': receiver}
         )
 
     def chat_message(self, event):
+        print('chat_message')
         message = event['message']
         self.send(text_data=json.dumps({
             'message': message,
-            'sender': event['sender'],
-            'receiver': event['receiver']
+            'sender_id': event['sender_id'],
+            'receiver_id': event['receiver_id']
         }))
