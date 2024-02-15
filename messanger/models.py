7c2dba258b547408269ac4b4f1aaaff017dc4b7a
Mikhail Markov <mikhaiil@mmarkovv.ru>
2024-02-15 17:26:08 +0300

    Implemented Message model

--- messanger/models.py
+++ messanger/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from home.models import MyUser
+
+
+class Message(models.Model):
+    """
+    Модель для объекта «сообщение».
+    """
+    class Meta:
+        db_table = 'message'
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+    sender_id = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='sender')
+    receiver_id = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='receiver')
+    title = models.CharField(max_length=250, verbose_name='Предпросмотр')
+    content = models.TextField(max_length=4096, verbose_name='Контент')
+    sent_datetime = models.DateTimeField(auto_now_add=True, verbose_name='Время отправки')
+    status = models.CharField(max_length=20, choices=(
+        ('sent', 'отправлено'),
+        ('delivered', 'доставлено'),
+        ('seen', 'прочитано'),
+        ('not_sent', 'не отправлено'),
+    ))
